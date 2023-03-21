28b83b12ef50fa4274697ab16e00e635fdad9ba4
Gabriel <gamatusevich@gmail.com>
2023-03-21 19:05:40 +0100

    feat: update actor.service.js with suggestions

--- src/services/actor.service.js
+++ src/services/actor.service.js
@@ -1,54 +1,70 @@
 import { Actor } from "../models/actor.model.js";
-import { Actor_Tvshow } from "../models/actor_tvshow.model.js";
+import { ActorTvshow } from "../models/actor_tvshow.model.js";
 import { Tvshow } from "../models/tvshow.model.js";
 import { Movie } from "../models/movie.model.js";
 
-export class ActorServices {
+/**
+ * The ActorService class takes care of interfacing with the Actor model to provide
+ * CRUD capabilities so that consumers can read/write the resource on permanent storage
+ */
+export class ActorService {
+    /**
+     * Fetches all Actors and returns an array of Actor instances
+     * @returns Actor[]
+     */
     async findAll() {
         try {
-            const actors = await Actor.findAll();
-            return actors;
-            
+            return await Actor.findAll();
         } catch (error) {
-            
             throw new Error(error);
         };
     };
 
+    /**
+     * Fetches a single Actor that matches the "id" parameter
+     * @param {string} id Identfier to find the Actor
+     * @returns Actor
+     */
     async findById(id){
-       
+        //TODO: might be word checking for more specific empty cases
+        if (!Boolean(id)) {
+            throw TypeError('you must provide an id to this function');
+        }
+
         try {
             const actor = await Actor.findOne({
                 where: 
                 {
                     id
                 },
-               
-                     include: [{
+                include: [
+                    {
                         model: Tvshow,
                         attributes: {exclude: ['id', 'directorId']},
-                      
-                     },
+                    },
                     {
-                        model: Movie,
-                        attributes: {exclude: ['id', 'directorId']},
-                     }],
-
-                    
+                    model: Movie,
+                    attributes: {exclude: ['id', 'directorId']},
+                    }
+                ],
                 attributes: {
                     exclude: ['id']
-                }
+                },
             });
+
             return actor;
-            
         } catch (error) {
             throw new Error(error);
-           
         };
     };
 
+    /**
+     * Creates a new Actor with the data parameters
+     * @param {*} data Actor properties to create the new Actor
+     * @returns Actor
+     */
     async create(data) {
-        console.log(data.tvshowId)
+        //TODO: validate required fields
         try {
             const newActor = await Actor.create({
                 name: data.name,
@@ -57,17 +73,18 @@ export class ActorServices {
                 awards: data.awards
             });
 
-            if(data.tvshowId){
-                await Actor_Tvshow.create({
+            //TODO: this function is creating a side effect, consider first checking for validity of TV show
+            if (data.tvshowId) {
+                await ActorTvshow.create({
                     actorId: newActor.id,
                     tvshowId: data.tvshowId
                 });
             }
 
+            //TODO: might be worth sending the tv show along as well in case it exists
             return newActor;
         } catch (error) {
             throw new Error(error);
         }
     }
-
 };
